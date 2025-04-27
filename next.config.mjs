52422f7fa6252d95fc3108e978becbbe34319dad
Shawn M. Crowley <scrowley@buffalo.edu>
2025-04-27 13:14:10 -0400

    Ran Prebuild

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
     reactStrictMode: true,
     assetPrefix: isProd ? '/shawnmcrowley.github.io/' : '',
-    basePath: isProd ? '/shawnmcrowley.github.io/' : '',
+    basePath: isProd ? '/shawnmcrowley.github.io' : '',
     output: 'export',
     rewrites: async () => [
       {
