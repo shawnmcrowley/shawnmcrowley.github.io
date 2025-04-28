e18b8867414dd23b32f2529c2eabd604855549bd
Shawn M. Crowley <scrowley@buffalo.edu>
2025-04-27 22:23:34 -0400

    Updated Docusauras Config

--- doc/docusaurus.config.js
+++ doc/docusaurus.config.js
@@ -66,13 +66,14 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'Portfolio',
+        title: 'DashBoard',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
+          to: '../', label: 'DashBoard'
         },
         items: [
-          {to: '/', label: 'Dashboard', position: 'left'},
+          
           {
             type: 'docSidebar',
             sidebarId: 'tutorialSidebar',
